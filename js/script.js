25b8213442de2b5856405c2066a922af1ccbd44f
Joel Wanjala <joel.n.wanjala@gmail.com>
2022-01-22 13:25:44 +0300

    Added media queries to make responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+/*
+// Select DOM elements
+const openNav = document.querySelector('.open-nav');
+const closeNav = document.querySelector('.close-nav');
+const navItem = document.querySelector('.navigation__item');
+const navList = document.querySelector('.navigation__list');
+
+// Functionality
+const showNavigation = function () {
+  // Hide the open nav button
+  openNav.classList.add('hidden');
+  closeNav.classList.remove('hidden');
+  navList.classList.remove('hidden');
+};
+
+const hideNavigation = function () {
+  // Hide the close nav button
+  openNav.classList.remove('hidden');
+  closeNav.classList.add('hidden');
+  navList.classList.add('hidden');
+};
+
+// Add event listeners
+openNav.addEventListener('click', showNavigation);
+closeNav.addEventListener('click', hideNavigation);
+navList.addEventListener('click', hideNavigation);
+
+// Init function
+window.addEventListener('load', function () {
+  closeNav.classList.add('hidden');
+});
+*/
